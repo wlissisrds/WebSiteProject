a54898de422e68795aebe67742bd3b3561973aee
wlissis <wlissisrds@outlook.com>
2021-09-07 10:57:29 -0300

    conectando banco de dados Mongodb

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
+if(process.env.NODE_ENV !== "production" ) {
+    require("dotenv").config()
+}
+
 const express = require("express");
 const app = express();
 const expressLayout = require("express-ejs-layouts");
@@ -10,8 +14,17 @@ app.set("layout","layouts/layout");
 
 app.use(expressLayout);
 app.use(express.static("public"));
-
 app.use("/",indexRouter);
+
+const mongoose = require("mongoose");
+mongoose.connect(process.env.DATABASE_URL   , {useNewUrlParser: true});
+
+const db = mongoose.connection
+//se encontrar um erro no banco de dados imprima a mensagem 'error'
+db.on('error', error => console.error(error));
+//quando abrimos o db imprima
+db.once('open', () => console.log("Connected to Mongoose"));
+
 
 
 app.listen(process.env.PORT || 3000);
