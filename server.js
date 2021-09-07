e88cd719353fc732ad611e7784a5dfc9292de40f
wlissis <wlissisrds@outlook.com>
2021-09-07 10:28:48 -0300

    render do arquivo ejs

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const expressLayout = require("express-ejs-layouts");
 
+const indexRouter = require("./routes/index");
+
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 app.set("layout","layouts/layout");
@@ -9,6 +11,7 @@ app.set("layout","layouts/layout");
 app.use(expressLayout);
 app.use(express.static("public"));
 
+app.use("/",indexRouter);
 
 
 app.listen(process.env.PORT || 3000);
